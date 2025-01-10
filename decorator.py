5fcb8e38d40d1b465d35ddf96eeaa26ac3ef9c47
Rohan <Rohan.dubey@simformsolutions.com>
2025-01-10 15:08:21 +0530

    Rectified Decorators

--- decorator.py
+++ decorator.py
@@ -1,11 +1,23 @@
-def decorator(function):
-    def wrapper_f():
+"""Create a decorator named time_it that measures the execution time of a function. Use this decorator to measure the time taken by a
+function calculate_sum that computes the sum of all numbers from 1 to ( n ).""" 
+
+import time
+
+def time_it(function):
+    def wrapper_f(*args,**kwargs):
+        start=time.time()
+        res=function(*args,**kwargs)
+        end=time.time()
+        total=end-start
         print("This is a decorator")
-        return function()
+        print("Time taken {} seconds".format(float(total)))
     return wrapper_f
 
-@decorator
-def hello():
-    print("Hello world")
+@time_it
+def sum(n):
+    sum=0
+    for a in range(n):
+        sum+=a
+    print(sum)
 
-hello()
+sum(100)
