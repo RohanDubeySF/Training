95e786ca39f9565acd56b5d8caafb9c6a246dd17
Rohan <Rohan.dubey@simformsolutions.com>
2025-01-13 18:45:52 +0530

    Added Math File

---
+++ Math/math.py
@@ -0,0 +1,25 @@
+import math
+
+A = min(5, 10,-5, 25)
+B = max(5, 10, 100, 25)
+
+print(A)
+print(B)
+
+C = abs(-7.25)
+print(C)
+
+d = pow(5, 5)
+print(d)
+
+e = math.sqrt(1024)
+print(e)
+
+f = math.ceil(1.4)
+g = math.floor(1.4)
+
+print(f) 
+print(g)
+
+p = math.pi
+print(p)
